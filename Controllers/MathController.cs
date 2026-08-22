25f4e2b18ea67f7ec8822b4993ce0811216f0abe
lea2018 <leannyr03@gmail.com>
2026-08-22 19:51:06 -0400

    Creando controlador para la API

---
+++ Controllers/MathController.cs
@@ -0,0 +1,17 @@
+using Microsoft.AspNetCore.Http.HttpResults;
+using Microsoft.AspNetCore.Mvc;
+
+namespace MiApi.AddControllers;
+
+[ApiController]
+[Route("api/[controller]")]
+public class MathController : ControllerBase
+{
+   [HttpGet("cuadrado/{numero:int}")] 
+   public IActionResult Cuadrado(int numero)
+    {
+        if (numero < 0)
+            return BadRequest("El numero debe ser mayor o igual a 0.");
+            return Ok($"La raiz cuadrada de {numero} es {numero * numero}");
+    }
+}
