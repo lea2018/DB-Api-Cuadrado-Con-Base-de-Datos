18968f785b9aa8abe77d434084007dc2f20c3998
lea2018 <leannyr03@gmail.com>
2026-08-30 22:58:36 -0400

    Creando carpetas en vs code

--- obj/Debug/net10.0/MiApiCuadrado.AssemblyInfo.cs
+++ obj/Debug/net10.0/MiApiCuadrado.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MiApiCuadrado")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+25f4e2b18ea67f7ec8822b4993ce0811216f0abe")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+75db97d4abea66a38eae823d3fa02ceafa42b440")]
 [assembly: System.Reflection.AssemblyProductAttribute("MiApiCuadrado")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MiApiCuadrado")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
